f7108579baf172a18c414312724723eb68a9ee95
kevinlobos42 <kevinlobos42@gmail.com>
2024-08-23 23:54:08 -0400

    .env integration

--- src/lib/firebaseConfig.js
+++ src/lib/firebaseConfig.js
@@ -3,13 +3,13 @@ import { getAuth } from 'firebase/auth';
 import { getFirestore } from 'firebase/firestore';
 
 const firebaseConfig = {
-	apiKey: "AIzaSyDFXWLHIdl_i6BYcIjN0B1kyplw8hyx4sA",
-	authDomain: "collaborative-text.firebaseapp.com",
-	projectId: "collaborative-text",
-	storageBucket: "collaborative-text.appspot.com",
-	messagingSenderId: "345825335337",
-	appId: "1:345825335337:web:5f4c8a37005e7c9761e438",
-	measurementId: "G-4YQ4JTF6PN"
+	apiKey: import.meta.env.VITE_FirebaseAPIKey,
+	authDomain: import.meta.env.VITE_AuthDomain,
+	projectId: import.meta.env.VITE_ProjectId,
+	storageBucket: import.meta.env.VITE_StorageBucket,
+	messagingSenderId: import.meta.env.VITE_MessagingSenderId,
+	appId: import.meta.env.VITE_AppId,
+	measurementId: import.meta.env.VITE_MeasurementId
 };
 
 const app = initializeApp(firebaseConfig)
